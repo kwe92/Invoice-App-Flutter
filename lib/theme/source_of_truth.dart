96da64ec860daaa5d8559499f7865003aeee5229
Kweayon  Clark <Kweayon@gmail.com>
2023-03-11 02:31:24 -0600

    Source of Truth

---
+++ lib/theme/source_of_truth.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Gaps {
+  const Gaps();
+  static const SizedBox gaph1 = SizedBox(height: 1);
+  static const SizedBox gaph2 = SizedBox(height: 2);
+  static const SizedBox gaph4 = SizedBox(height: 4);
+  static const SizedBox gaph6 = SizedBox(height: 6);
+  static const SizedBox gaph8 = SizedBox(height: 8);
+  static const SizedBox gaph10 = SizedBox(height: 10);
+  static const SizedBox gaph12 = SizedBox(height: 12);
+  static const SizedBox gaph14 = SizedBox(height: 14);
+  static const SizedBox gaph16 = SizedBox(height: 16);
+  static const SizedBox gaph18 = SizedBox(height: 18);
+
+  static const SizedBox gapw1 = SizedBox(width: 1);
+  static const SizedBox gapw2 = SizedBox(width: 2);
+  static const SizedBox gapw4 = SizedBox(width: 4);
+  static const SizedBox gapw6 = SizedBox(width: 6);
+  static const SizedBox gapw8 = SizedBox(width: 8);
+  static const SizedBox gapw10 = SizedBox(width: 10);
+  static const SizedBox gapw12 = SizedBox(width: 12);
+  static const SizedBox gapw14 = SizedBox(width: 14);
+  static const SizedBox gapw16 = SizedBox(width: 16);
+  static const SizedBox gapw18 = SizedBox(width: 18);
+
+  static SizedBox width(double w) => SizedBox(width: w);
+  static SizedBox heigth(double h) => SizedBox(height: h);
+}
