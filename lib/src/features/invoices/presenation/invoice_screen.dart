65c234717a79cde3b0f430742e97a3d0ced9c1b6
Kweayon  Clark <Kweayon@gmail.com>
2023-03-12 21:24:34 -0500

    Refactor

--- lib/src/features/invoices/presenation/invoice_screen.dart
+++ lib/src/features/invoices/presenation/invoice_screen.dart
@@ -1,4 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/titte_icon_list_tile.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
+
+// const double sqrVal = 38;
+// const double factor = 2.25;
+
+class InvoiceScreen extends StatelessWidget {
+  const InvoiceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: const <Widget>[TittleIconListTile()],
+    );
+  }
+}
