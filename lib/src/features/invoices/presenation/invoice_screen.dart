72aa5e9e58a5202ee4ab7015f9d4041de20fcfad
Kweayon  Clark <Kweayon@gmail.com>
2023-03-13 13:58:09 -0500

    Invoice Screen Update

--- lib/src/features/invoices/presenation/invoice_screen.dart
+++ lib/src/features/invoices/presenation/invoice_screen.dart
@@ -17,19 +17,93 @@ class InvoiceScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[const TittleIconListTile(), _invoiceListTile()],
+    return Expanded(
+      child: ListView(
+        children: <Widget>[
+          const TittleIconListTile(),
+          for (int i = 0; i < 12; i++) _invoiceListTile()
+        ],
+      ),
     );
   }
 }
 
 Widget _invoiceListTile() {
+  const double offsetVal = 2;
+  const double blur = 18;
+  final shadowColor = CustomTheme.lightColors['shade2'] as Color;
   return Padding(
-    padding: const EdgeInsets.symmetric(
-        horizontal: CustomTheme.mainContentPadding, vertical: 24.0),
+    // padding:
+    //     const EdgeInsets.symmetric(horizontal: CustomTheme.mainContentPadding),
+    padding: const EdgeInsets.only(
+        left: CustomTheme.mainContentPadding,
+        right: CustomTheme.mainContentPadding,
+        top: 24.0,
+        bottom: 0),
     child: Container(
-      height: 132,
-      decoration: const BoxDecoration(color: Colors.lightBlue),
+      height: 142,
+      decoration: BoxDecoration(
+        color: CustomTheme.lightColors['shade3'],
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: shadowColor,
+            offset: const Offset(offsetVal, offsetVal),
+            blurRadius: blur,
+            spreadRadius: 2.0,
+          ),
+          BoxShadow(
+            color: shadowColor,
+            offset: const Offset(offsetVal * -1, offsetVal * -1),
+            blurRadius: blur,
+            spreadRadius: 2.0,
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[_listTileLeftSide(), _rightSide()],
+      ),
     ),
+  );
+}
+
+Widget _listTileLeftSide() => Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const <Widget>[
+        Text('#RT3080'),
+        Text('Due 19 Aug 2021'),
+        Text('\$1,800.90'),
+      ],
+    );
+
+Widget _rightSide() {
+  const double circleSize = 10;
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: <Widget>[
+      const Text('Baki Hanma'),
+      Container(
+        color: const Color.fromARGB(50, 118, 238, 122),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          child: Row(children: <Widget>[
+            Container(
+              width: circleSize,
+              height: circleSize,
+              decoration: const BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(50),
+                ),
+              ),
+            ),
+            Gaps.gapw10,
+            const Text('Pending')
+          ]),
+        ),
+      )
+    ],
   );
 }
