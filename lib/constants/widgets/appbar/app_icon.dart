cce7e32606334802655b5c6d6bac9f8e9db9b1d2
Kweayon  Clark <Kweayon@gmail.com>
2023-03-15 23:39:52 -0500

    Upate

--- lib/constants/widgets/appbar/app_icon.dart
+++ lib/constants/widgets/appbar/app_icon.dart
@@ -6,6 +6,7 @@ import 'package:invoiceapp/theme/theme.dart';
 
 class AppIcon extends StatelessWidget {
   final Radius radius;
+
   const AppIcon({required this.radius, super.key});
 
   @override
@@ -24,51 +25,72 @@ class AppIcon extends StatelessWidget {
           ),
           height: 60,
         ),
-        _icon(radius: radius, color: topColorIconBox),
+        _Icon(radius: radius, color: topColorIconBox),
       ],
     );
   }
 }
 
-Widget _icon({required Radius radius, required Color color}) {
-  return Stack(children: [
-    Center(
-        child: Column(children: [
-      _topBox(color: color, radius: radius),
-      _bottomBox(radius: radius),
-    ])),
-    _iconImage()
-  ]);
+class _Icon extends StatelessWidget {
+  final Radius radius;
+  final Color color;
+
+  const _Icon({required this.radius, required this.color, super.key});
+
+  @override
+  Widget build(BuildContext context) => Stack(children: [
+        Center(
+            child: Column(children: [
+          _TopBox(color: color, radius: radius),
+          _BottomBox(radius: radius),
+        ])),
+        _iconImage
+      ]);
 }
 
-Container _topBox({required Color color, required Radius radius}) => Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.only(
-          topRight: radius,
-        ),
-      ),
-      height: 28,
-      width: 60,
-    );
+class _TopBox extends StatelessWidget {
+  final Color color;
+  final Radius radius;
 
-Container _bottomBox({required Radius radius}) => Container(
-      decoration: BoxDecoration(
-        color: CustomTheme.otherColors['purple1'] as Color,
-        borderRadius: BorderRadius.only(
-          bottomRight: radius,
-          topLeft: radius,
-        ),
-      ),
-      height: 28,
-      width: 60,
-    );
+  const _TopBox({required this.color, required Radius this.radius, super.key});
 
-Container _iconImage() => Container(
-      alignment: Alignment.center,
-      child: SvgPicture.asset(
-        'assets/logo.svg',
-        colorFilter: ColorFilter.mode(
-            CustomTheme.lightColors['shade3'] as Color, BlendMode.srcIn),
-      ),
-    );
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.only(
+            topRight: radius,
+          ),
+        ),
+        height: 28,
+        width: 60,
+      );
+}
+
+class _BottomBox extends StatelessWidget {
+  final Radius radius;
+
+  const _BottomBox({required this.radius, super.key});
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: BoxDecoration(
+          color: CustomTheme.otherColors['purple1'] as Color,
+          borderRadius: BorderRadius.only(
+            bottomRight: radius,
+            topLeft: radius,
+          ),
+        ),
+        height: 28,
+        width: 60,
+      );
+}
+
+final _iconImage = Container(
+  alignment: Alignment.center,
+  child: SvgPicture.asset(
+    'assets/logo.svg',
+    colorFilter: ColorFilter.mode(
+        CustomTheme.lightColors['shade3'] as Color, BlendMode.srcIn),
+  ),
+);
