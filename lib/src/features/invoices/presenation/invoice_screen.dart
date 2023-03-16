1334a8659f0598d46d490f771a705c958fa63149
Kweayon  Clark <Kweayon@gmail.com>
2023-03-16 16:17:37 -0500

    Refactor

--- lib/src/features/invoices/presenation/invoice_screen.dart
+++ lib/src/features/invoices/presenation/invoice_screen.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:invoiceapp/src/features/invoices/data/invoice.dart';
-import 'package:invoiceapp/src/features/invoices/presenation/tittle_icon_list_tile.dart';
-import 'package:invoiceapp/theme/source_of_truth.dart';
-import 'package:invoiceapp/theme/theme.dart';
-
-// const double sqrVal = 38;
-// const double factor = 2.25;
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_error_screen.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/title_icon_list_tile.dart';
 
 final _invoice = Invoice(
   id: '#RT3080',
@@ -21,176 +17,12 @@ class InvoiceScreen extends StatelessWidget {
   const InvoiceScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView(
-        children: <Widget>[
-          const TittleIconListTile(),
-          // for (int i = 0; i < 12; i++) _invoiceListTile()
-          _errorScreen()
-        ],
-      ),
-    );
-  }
+  Widget build(BuildContext context) =>
+      Expanded(child: ListView(children: _screenBody));
 }
 
-Widget _invoiceListTile() {
-  const double offsetVal = 2;
-  const double blur = 12;
-  final shadowColor = CustomTheme.lightColors['shade2'] as Color;
-  return Padding(
-    padding: const EdgeInsets.only(
-        left: CustomTheme.mainContentPadding,
-        right: CustomTheme.mainContentPadding,
-        top: 24.0,
-        bottom: 0),
-    child: Container(
-      height: 142,
-      decoration: BoxDecoration(
-        color: CustomTheme.lightColors['shade3'],
-        boxShadow: <BoxShadow>[
-          BoxShadow(
-            color: shadowColor,
-            offset: const Offset(offsetVal, offsetVal),
-            blurRadius: blur,
-          ),
-          BoxShadow(
-            color: shadowColor,
-            offset: const Offset(offsetVal * -1, offsetVal * -1),
-            blurRadius: blur,
-          ),
-        ],
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 17.25),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[_listTileLeftSide(), _rightSide()],
-        ),
-      ),
-    ),
-  );
-}
-
-Widget _listTileLeftSide() {
-  final List<Widget> invoceNumber = [
-    Text(
-      _invoice.id.substring(0, 1),
-      style: TextStyle(color: CustomTheme.lightColors['shade0']),
-    ),
-    Text(_invoice.id.substring(1))
-  ];
-
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: invoceNumber,
-      ),
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            'Due ${_invoice.date.day} ${_getStringMonth(_invoice.date.month)} ${_invoice.date.year}',
-            style: TextStyle(color: CustomTheme.lightColors['shade0']),
-          ),
-          Gaps.gaph8,
-          // TODO: How to convert a duble into a currency representation??
-          const Text('\$1,800.90'),
-        ],
-      )
-    ],
-  );
-}
-
-Widget _rightSide() {
-  final TextStyle nameStyle =
-      TextStyle(color: CustomTheme.lightColors['shade0']);
-  const double circleSize = 10;
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    crossAxisAlignment: CrossAxisAlignment.end,
-    children: <Widget>[
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          Text(
-            'Baki',
-            style: nameStyle,
-          ),
-          Text(
-            'Hanma',
-            style: nameStyle,
-          )
-        ],
-      ),
-      Container(
-        height: 46.75,
-        width: 108,
-        color: const Color.fromARGB(28, 118, 238, 122),
-        child:
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Container(
-            width: circleSize,
-            height: circleSize,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 81, 215, 85),
-              borderRadius: BorderRadius.all(
-                Radius.circular(50),
-              ),
-            ),
-          ),
-          Gaps.gapw10,
-          const Text(
-            'Pending',
-            style: TextStyle(color: Color.fromARGB(255, 92, 205, 95)),
-          )
-        ]),
-      )
-    ],
-  );
-}
-
-String _getStringMonth(int month) {
-  List<String> months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'June',
-    'July',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Nov',
-    'Dec'
-  ];
-
-  return months.elementAt(month);
-}
-
-Widget _errorScreen() => Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(top: 34.25, bottom: 32),
-          child: SizedBox(
-              child: SvgPicture.asset('assets/illustration-empty.svg')),
-        ),
-        const Text(
-          'There is nothing here',
-          style: TextStyle(
-              fontSize: 26, fontWeight: FontWeight.w500, letterSpacing: -0.5),
-        ),
-        Gaps.gaph16,
-        SizedBox(
-            width: 206,
-            child: Text(
-              'Click the new button to get started creating an invoice',
-              style: TextStyle(
-                  fontSize: 16, color: CustomTheme.lightColors['shade0']),
-            )),
-      ],
-    );
+final _screenBody = <Widget>[
+  const TitleIconListTile(),
+  for (int i = 0; i < 12; i++) InvoiceListTile(invoice: _invoice),
+  const ErrorScreen()
+];
