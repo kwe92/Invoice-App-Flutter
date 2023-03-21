4e58f63cd4b02a8a1b4cba98b3cc379938e71560
Kwe <Kweayon@gmail.com>
2023-03-21 18:38:11 -0500

    Refactor DRY

--- lib/src/features/newInvoice/presentation/bill_from.dart
+++ lib/src/features/newInvoice/presentation/bill_from.dart
@@ -1,51 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/presentation/custom_text_form_field.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
+import 'package:provider/provider.dart';
 
-class BillForm extends StatefulWidget {
+final _formKey = GlobalKey<FormState>();
+
+class BillForm extends StatelessWidget {
   const BillForm({super.key});
-
-  @override
-  State<BillForm> createState() => _BillFormState();
-}
-
-class _BillFormState extends State<BillForm> {
-  final _formKey = GlobalKey<FormState>();
-
-  TextEditingController streetAddController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final purple = CustomTheme.otherColors['purple0'] as Color;
-    final grey = CustomTheme.lightColors['shade1'] as Color;
+    final gaph = Gaps.heigth(24);
+
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 18.0),
       child: Form(
         key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Bill From', style: TextStyle(fontSize: 18, color: purple)),
-            Gaps.heigth(24),
-            TextFormField(
-              controller: streetAddController,
-              decoration: InputDecoration(
-                label: Text(
-                  'Street Address',
-                  style: TextStyle(color: grey),
-                ),
-                // labelText: 'Street Address',
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        color: CustomTheme.lightColors['shade1'] as Color),
-                    borderRadius: BorderRadius.circular(6)),
-                focusedBorder:
-                    OutlineInputBorder(borderSide: BorderSide(color: purple)),
+        child: Consumer<BillFromModel>(
+          builder: (context, billFrom, child) => Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Bill From', style: TextStyle(fontSize: 18, color: purple)),
+              gaph,
+              CustomTextFormField(
+                  title: 'Street Address',
+                  controller: billFrom.streetAddController),
+              gaph,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 165,
+                    child: CustomTextFormField(
+                        title: 'City', controller: billFrom.cityController),
+                  ),
+                  SizedBox(
+                    width: 165,
+                    child: CustomTextFormField(
+                        title: 'Post Code',
+                        controller: billFrom.postCodeController),
+                  ),
+                ],
               ),
-            )
-          ],
+              gaph,
+              CustomTextFormField(
+                  title: 'Country', controller: billFrom.countryController),
+              OutlinedButton(
+                  onPressed: () {
+                    _clearButton(billFrom);
+                  },
+                  child: const Text('Clear Text'))
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+void _clearButton(BillFromModel model) {
+  model.clearCityController();
+  model.clearCountryController();
+  model.clearPostCodeController();
+  model.clearStreetController();
 }
