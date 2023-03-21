81d3e31d0d08053a3480dd501bc7c9f65128403c
Kwe <Kweayon@gmail.com>
2023-03-21 18:37:59 -0500

    Refactor DRY

---
+++ lib/src/features/newInvoice/presentation/custom_text_form_field.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:invoiceapp/theme/theme.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String title;
+  final TextEditingController controller;
+  const CustomTextFormField(
+      {required this.title, required this.controller, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final purple = CustomTheme.otherColors['purple0'] as Color;
+    final grey = CustomTheme.lightColors['shade1'] as Color;
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        label: Text(
+          title,
+          style: TextStyle(color: grey),
+        ),
+        // labelText: 'Street Address',
+        border: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: CustomTheme.lightColors['shade1'] as Color),
+            borderRadius: BorderRadius.circular(6)),
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: purple)),
+      ),
+    );
+  }
+}
