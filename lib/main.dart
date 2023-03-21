be1e34581cb99929f28c4f807a76c9f603fca960
Kwe <Kweayon@gmail.com>
2023-03-21 18:37:01 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:invoiceapp/app.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/new_invoice.dart';
+import 'package:provider/provider.dart';
 
 GoRouter _router = GoRouter(routes: <GoRoute>[
   GoRoute(path: '/', builder: _homePath, routes: <GoRoute>[
@@ -12,8 +14,11 @@ GoRouter _router = GoRouter(routes: <GoRoute>[
   ]),
 ]);
 
-void main() => runApp(MaterialApp.router(
-      routerConfig: _router,
+void main() => runApp(ChangeNotifierProvider(
+      create: (context) => BillFromModel(),
+      child: MaterialApp.router(
+        routerConfig: _router,
+      ),
     ));
 
 Widget _homePath(BuildContext context, GoRouterState state) => const MyApp();
