f4a4244322911970b818c8f80917c6d035e65637
Kwe <Kweayon@gmail.com>
2023-03-21 18:37:23 -0500

    Refactor

--- lib/src/features/newInvoice/presentation/new_invoice.dart
+++ lib/src/features/newInvoice/presentation/new_invoice.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:go_router/go_router.dart';
-import 'package:invoiceapp/constants/widgets/appbar/appbar.dart';
 import 'package:invoiceapp/constants/widgets/base_scaffold/base_scaffold.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/bill_from.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
@@ -17,35 +15,18 @@ class NewInvoice extends StatelessWidget {
       padding: CustomTheme.mainContentPadding,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          _backButton(context),
+        children: const <Widget>[
+          BackButton(),
           Gaps.gaph18,
-          _txt,
-          const BillForm()
+          _title,
+          BillForm(),
         ],
       ),
     ));
   }
 }
 
-const _txt = Text(
+const _title = Text(
   'New Invoice',
   style: TextStyle(fontSize: 26, fontWeight: FontWeight.w400),
 );
-
-Widget _backButton(BuildContext context) => GestureDetector(
-      onTap: () => context.pushReplacement('/'),
-      child: Row(
-        // mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SvgPicture.asset(
-            'assets/icon-arrow-left.svg',
-          ),
-          Gaps.gapw8,
-          const Text(
-            'Go Back',
-            style: TextStyle(fontSize: 16),
-          )
-        ],
-      ),
-    );
