26ccc9872f2fb14fa8d4bab4faea1b677b60b202
Kwe <Kweayon@gmail.com>
2023-03-21 18:37:13 -0500

    Update

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/constants/widgets/appbar/appbar.dart';
+import 'package:invoiceapp/constants/widgets/base_scaffold/base_scaffold.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_screen.dart';
 import 'package:invoiceapp/theme/theme.dart';
 
@@ -7,14 +8,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) => SafeArea(
-        child: Scaffold(
-            backgroundColor: CustomTheme.lightColors['shade2`'],
-            appBar: const CustomAppBar(
-              preferredSize: CustomTheme.appBarHeight,
-            ),
-            body: _body),
-      );
+  Widget build(BuildContext context) => BaseScaffold(body: _body);
 }
 
 final Widget _body = Padding(
