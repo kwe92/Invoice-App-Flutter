13a88a41d56a2e5dd54ed2c5725d6d91bb7bb41f
Kwe <Kweayon@gmail.com>
2023-03-21 18:38:38 -0500

    Created Back Button Component

---
+++ lib/src/features/newInvoice/presentation/back_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:invoiceapp/theme/source_of_truth.dart';
+
+class BackButton extends StatelessWidget {
+  const BackButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => context.pushReplacementNamed('/'),
+      child: Row(
+        // mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          SvgPicture.asset(
+            'assets/icon-arrow-left.svg',
+          ),
+          Gaps.gapw8,
+          const Text(
+            'Go Back',
+            style: TextStyle(fontSize: 16),
+          )
+        ],
+      ),
+    );
+  }
+}
