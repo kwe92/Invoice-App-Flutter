7061f9eda315232adab158316dc14b3eb9555ae4
Kwe <Kweayon@gmail.com>
2023-03-21 01:17:52 -0500

    Updated Base Scaffold

--- lib/constants/widgets/base_scaffold/base_scaffold.dart
+++ lib/constants/widgets/base_scaffold/base_scaffold.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:invoiceapp/constants/widgets/appbar/appbar.dart';
+import 'package:invoiceapp/theme/theme.dart';
+
+// TODO: Refactor Invoices and NewInvoices to use the same BaseScaffold for appbar nad background
+
+class BaseScaffold extends StatelessWidget {
+  final Widget body;
+  const BaseScaffold({required this.body, super.key});
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+            backgroundColor: CustomTheme.lightColors['shade3'],
+            appBar: const CustomAppBar(
+              preferredSize: CustomTheme.appBarHeight,
+            ),
+            body: body),
+      );
+}
