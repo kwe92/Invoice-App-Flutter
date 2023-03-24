df1e195f6dcd33820d86865b8fd460f1025b955d
Kwe <Kweayon@gmail.com>
2023-03-24 12:56:24 -0500

    Implemented Router Class

---
+++ lib/constants/router/app_router.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_screen.dart';
+import 'package:invoiceapp/src/features/newInvoice/presentation/new_invoice.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(routes: <GoRoute>[
+    GoRoute(path: '/', builder: _homePath, routes: <GoRoute>[
+      GoRoute(
+        path: 'newInvoice',
+        builder: (context, state) => const NewInvoice(),
+      )
+    ]),
+  ]);
+}
+
+Widget _homePath(BuildContext context, GoRouterState state) =>
+    const InvoiceScreen();
