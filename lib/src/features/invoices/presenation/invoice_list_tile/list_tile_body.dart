d77ef8fb3e70bd1a16fc7f32b251379ff3a890ad
Kwe <Kweayon@gmail.com>
2023-03-24 11:22:37 -0500

    Decouple Invoice List Tile

---
+++ lib/src/features/invoices/presenation/invoice_list_tile/list_tile_body.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:invoiceapp/src/features/invoices/data/invoice.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/list_tile_decoration.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/list_tile_left_side.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/list_tile_right_side.dart';
+
+class InvoiceListTileBody extends StatelessWidget {
+  final Invoice invoice;
+  const InvoiceListTileBody({required this.invoice, super.key});
+
+  @override
+  Widget build(BuildContext context) => Container(
+        height: 142,
+        decoration: CustomDecoration.decoration(),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 17.25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              ListTileLeftSide(invoice: invoice),
+              const ListTileRightSide()
+            ],
+          ),
+        ),
+      );
+}
