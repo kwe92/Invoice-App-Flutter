db694c5afa4f46546b48125c4994e7b2c8b3212a
Kwe <Kweayon@gmail.com>
2023-03-24 11:22:46 -0500

    Decouple Invoice List Tile

---
+++ lib/src/features/invoices/presenation/invoice_list_tile/list_tile_decoration.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+import 'package:invoiceapp/theme/theme.dart';
+
+class CustomDecoration {
+  const CustomDecoration();
+
+  static decoration() {
+    const double offsetVal = 2;
+    const double blur = 12;
+    final Color shadowColor = CustomTheme.lightColors['shade2'] as Color;
+    return BoxDecoration(
+      color: CustomTheme.lightColors['shade3'],
+      boxShadow: <BoxShadow>[
+        BoxShadow(
+          color: shadowColor,
+          offset: const Offset(offsetVal, offsetVal),
+          blurRadius: blur,
+        ),
+        BoxShadow(
+          color: shadowColor,
+          offset: const Offset(offsetVal * -1, offsetVal * -1),
+          blurRadius: blur,
+        ),
+      ],
+    );
+  }
+}
