8d2c847e131220cac8f0feccc3457bbd35db5a6c
Kwe <Kweayon@gmail.com>
2023-03-24 11:23:22 -0500

    Decouple Invoice List Tile

--- lib/src/features/invoices/presenation/invoice_screen.dart
+++ lib/src/features/invoices/presenation/invoice_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/src/features/invoices/data/invoice.dart';
-import 'package:invoiceapp/src/features/invoices/presenation/invoice_error_screen.dart';
-import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/invoice_list_tile.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/title_icon_list_tile.dart';
 
 final Invoice _invoice = Invoice(
@@ -17,12 +16,10 @@ class InvoiceScreen extends StatelessWidget {
   const InvoiceScreen({super.key});
 
   @override
-  Widget build(BuildContext context) =>
-      Expanded(child: ListView(children: _screenBody));
+  Widget build(BuildContext context) => ListView(children: _screenBody);
 }
 
 final _screenBody = <Widget>[
   const TitleIconListTile(),
   for (int i = 0; i < 12; i++) InvoiceListTile(invoice: _invoice),
-  const ErrorScreen()
 ];
