df38a138919b0af2aaf68cb873b1f1a4e9962af8
Kwe <Kweayon@gmail.com>
2023-03-24 21:44:43 -0500

    Added more tests

--- test/src/features/invoices/presenation/invoice_screen_test.dart
+++ test/src/features/invoices/presenation/invoice_screen_test.dart
@@ -5,6 +5,8 @@ import 'package:invoiceapp/constants/router/app_router.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_screen.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/title_icon_list_tile.dart';
 import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/presentation/bill_from.dart';
+import 'package:invoiceapp/src/features/newInvoice/presentation/new_invoice.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -22,11 +24,33 @@ void main() {
     final baseScaffold = find.byKey(InvoiceScreen.baseScaffoldKey);
     expect(baseScaffold, findsOneWidget);
 
-    final invoiceButton = find.byKey(TitleIconListTile.newInvoiceButtonKey);
+    final titleIconListTile = find.byKey(InvoiceScreen.titleIconListTileKey);
+    expect(titleIconListTile, findsOneWidget);
+
+    final invoiceButton = find.descendant(
+        of: titleIconListTile,
+        matching: find.byKey(TitleIconListTile.newInvoiceButtonKey));
     expect(invoiceButton, findsOneWidget);
 
     await tester.tap(invoiceButton);
-    tester.pumpAndSettle();
+    await tester.pumpAndSettle();
+
+    // //TODO: Figure out why navigation is not happening
+
+    // final newInvoiceScreen = find.byKey(const Key('newInvocieKey'));
+    // expect(newInvoiceScreen, findsOneWidget);
+
+    // final billForm = find.byKey(NewInvoice.billFormKey);
+    // expect(billForm, findsOneWidget);
+
+    // final form = find.byKey(BillForm.formKey);
+    // expect(billForm, billForm);
+
+    // expect(find.text('City'), findsOneWidget);
+
+    // final cityKey = find.byKey(BillForm.cityKey);
+    // final cityInput = find.descendant(of: billForm, matching: cityKey);
+    // expect(cityInput, findsOneWidget);
   });
 }
 
