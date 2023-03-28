100e84ec79115f5aec28756db4d46bb7bdd3427d
Kwe <Kweayon@gmail.com>
2023-03-27 20:05:29 -0500

    Added Multiple Providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/constants/router/app_router.dart';
-import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_form_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_to_model.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: _notifier,
+  // runApp(ChangeNotifierProvider(
+  //   create: _notifier,
+  //   child: _child,
+  // ));
+
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: _billFromNotifier,
+      ),
+      ChangeNotifierProvider(
+        create: _billToNotifier,
+      )
+    ],
     child: _child,
   ));
 }
@@ -13,4 +26,6 @@ void main() {
 final _child = MaterialApp.router(
   routerConfig: AppRouter.router,
 );
-BillFromModel _notifier(context) => BillFromModel();
+BillFromModel _billFromNotifier(context) => BillFromModel();
+
+BillToModel _billToNotifier(context) => BillToModel();
