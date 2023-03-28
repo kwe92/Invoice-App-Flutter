a47b9c17dc0ae09adf64f85468590a757869ddab
Kwe <Kweayon@gmail.com>
2023-03-28 18:44:00 -0500

    Update

--- lib/src/features/newInvoice/presentation/new_invoice.dart
+++ lib/src/features/newInvoice/presentation/new_invoice.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/constants/widgets/base_scaffold/base_scaffold.dart';
 import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_to_model.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/back_button.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/bill_from.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/bill_to.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
+import 'package:provider/provider.dart';
 
 class NewInvoice extends StatelessWidget {
   static const baseScaffoldKey = Key('baseScaffoldKey');
@@ -20,17 +22,26 @@ class NewInvoice extends StatelessWidget {
         body: Padding(
           padding: CustomTheme.mainContentPadding,
           child: SingleChildScrollView(
-            child: Column(
+              child: Consumer2(
+            builder: (context, BillFromModel bfm, BillToModel btm, child) =>
+                Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: const <Widget>[
-                CustomBackButton(),
+              children: <Widget>[
+                const CustomBackButton(),
                 Gaps.gaph18,
                 _title,
-                BillFrom(key: NewInvoice.billFromKey),
-                // BillTo(),
+                BillFrom(model: bfm, key: NewInvoice.billFromKey),
+                BillTo(model: btm),
+                OutlinedButton(
+                    key: BillFrom.clearTextKey,
+                    onPressed: () {
+                      bfm.clearAllControllers();
+                      btm.clearAllControllers();
+                    },
+                    child: const Text('Clear Text'))
               ],
             ),
-          ),
+          )),
         ),
       );
 }
