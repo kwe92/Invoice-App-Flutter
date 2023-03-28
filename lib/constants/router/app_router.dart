bac6b728b4506d3c018816fadbfd9006fe10c770
Kwe <Kweayon@gmail.com>
2023-03-28 18:09:20 -0500

    Added Dependency injection GetIt

--- lib/constants/router/app_router.dart
+++ lib/constants/router/app_router.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_screen.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/new_invoice.dart';
+import 'package:invoiceapp/src/features/shared/services/get_it.dart';
+
+AppRouter get appRouter {
+  return getIt.get<AppRouter>();
+}
 
 class AppRouter {
-  static final GoRouter router = GoRouter(routes: <GoRoute>[
+  final GoRouter router = GoRouter(routes: <GoRoute>[
     GoRoute(path: '/', builder: _homePath, routes: <GoRoute>[
       GoRoute(
         path: 'newInvoice',
