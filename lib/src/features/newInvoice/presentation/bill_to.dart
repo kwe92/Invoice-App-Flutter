70bc0926bcea2f7080d14055af4dcd6f1f9d28f4
Kwe <Kweayon@gmail.com>
2023-03-28 18:44:08 -0500

    Update

--- lib/src/features/newInvoice/presentation/bill_to.dart
+++ lib/src/features/newInvoice/presentation/bill_to.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
 import 'package:invoiceapp/src/features/newInvoice/domain/bill_to_model.dart';
-import 'package:invoiceapp/src/features/newInvoice/presentation/bill_from.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/custom_text_form_field.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
-import 'package:provider/provider.dart';
 
 // final _formKey = GlobalKey<FormState>();
 
@@ -20,7 +17,8 @@ class BillTo extends StatelessWidget {
   static const clientEmailKey = Key('clientEmailKey');
   static const projectDescKey = Key('projectDescKey');
 
-  const BillTo({super.key});
+  final BillToModel model;
+  const BillTo({required this.model, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -32,67 +30,61 @@ class BillTo extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 18.0),
       child: Form(
         key: BillTo.formKey,
-        child: Consumer<BillToModel>(
-          builder: (context, model, child) => Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Bill To', style: TextStyle(fontSize: 18, color: purple)),
-              gaph,
-              CustomTextFormField(
-                key: BillTo.clientNameKey,
-                title: "Client's Name",
-                controller: model.clientNameController,
-              ),
-              gaph,
-              CustomTextFormField(
-                key: BillTo.clientEmailKey,
-                title: "Client's Email",
-                controller: model.clientEmailController,
-              ),
-              gaph,
-              CustomTextFormField(
-                key: BillTo.streetAddressKey,
-                title: 'Street Address',
-                controller: model.streetAddController,
-              ),
-              gaph,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: textInputWidth,
-                    child: CustomTextFormField(
-                        key: BillTo.cityKey,
-                        title: 'City',
-                        controller: model.cityController),
-                  ),
-                  SizedBox(
-                    width: textInputWidth,
-                    child: CustomTextFormField(
-                        key: BillTo.postCodeKey,
-                        title: 'Post Code',
-                        controller: model.postCodeController),
-                  ),
-                ],
-              ),
-              gaph,
-              CustomTextFormField(
-                  key: BillTo.countryKey,
-                  title: 'Country',
-                  controller: model.countryController),
-              gaph,
-              CustomTextFormField(
-                key: BillTo.projectDescKey,
-                title: 'Project Description',
-                controller: model.projectDescController,
-              ),
-              gaph,
-              OutlinedButton(
-                  key: BillTo.clearTextKey,
-                  onPressed: () => model.clearAllControllers(),
-                  child: const Text('Clear Text'))
-            ],
-          ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Bill To', style: TextStyle(fontSize: 18, color: purple)),
+            gaph,
+            CustomTextFormField(
+              key: BillTo.clientNameKey,
+              title: "Client's Name",
+              controller: model.clientNameController,
+            ),
+            gaph,
+            CustomTextFormField(
+              key: BillTo.clientEmailKey,
+              title: "Client's Email",
+              controller: model.clientEmailController,
+            ),
+            gaph,
+            CustomTextFormField(
+              key: BillTo.streetAddressKey,
+              title: 'Street Address',
+              controller: model.streetAddController,
+            ),
+            gaph,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  width: textInputWidth,
+                  child: CustomTextFormField(
+                      key: BillTo.cityKey,
+                      title: 'City',
+                      controller: model.cityController),
+                ),
+                SizedBox(
+                  width: textInputWidth,
+                  child: CustomTextFormField(
+                      key: BillTo.postCodeKey,
+                      title: 'Post Code',
+                      controller: model.postCodeController),
+                ),
+              ],
+            ),
+            gaph,
+            CustomTextFormField(
+                key: BillTo.countryKey,
+                title: 'Country',
+                controller: model.countryController),
+            gaph,
+            CustomTextFormField(
+              key: BillTo.projectDescKey,
+              title: 'Project Description',
+              controller: model.projectDescController,
+            ),
+            gaph,
+          ],
         ),
       ),
     );
