ac4b08a12acddf8007b3e1055b27e6b6820c8236
Kwe <Kweayon@gmail.com>
2023-03-29 12:41:41 -0500

    Added Nullable onTap

--- lib/src/features/newInvoice/presentation/custom_text_form_field.dart
+++ lib/src/features/newInvoice/presentation/custom_text_form_field.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/theme/theme.dart';
 
+typedef VoidCallback = Function();
+
 class CustomTextFormField extends StatelessWidget {
   final String title;
   final TextEditingController controller;
+
+  final VoidCallback? onTap;
+
   const CustomTextFormField(
-      {required this.title, required this.controller, super.key});
+      {required this.title, required this.controller, this.onTap, super.key});
 
   @override
   Widget build(BuildContext context) {
     final purple = CustomTheme.otherColors['purple0'] as Color;
     final grey = CustomTheme.lightColors['shade1'] as Color;
     return TextFormField(
+      onTap: onTap,
       controller: controller,
       decoration: InputDecoration(
         focusColor: purple,
