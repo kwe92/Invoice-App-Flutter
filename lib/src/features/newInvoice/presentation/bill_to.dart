33c2c2c0737dcabb9f2fbc1c89342bcd83df7671
Kwe <Kweayon@gmail.com>
2023-03-30 21:05:35 -0500

    Update

--- lib/src/features/newInvoice/presentation/bill_to.dart
+++ lib/src/features/newInvoice/presentation/bill_to.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/src/features/newInvoice/domain/bill_to_model.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/custom_text_form_field.dart';
+import 'package:invoiceapp/src/features/newInvoice/presentation/drop_down_menu.dart';
+import 'package:invoiceapp/src/features/newInvoice/presentation/list_item.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
 
@@ -25,88 +27,94 @@ class BillTo extends StatelessWidget {
   Widget build(BuildContext context) {
     final purple = CustomTheme.otherColors['purple0'] as Color;
     final gaph = Gaps.heigth(18);
+    final textStyle = TextStyle(fontSize: 18, color: purple);
     const double textInputWidth = 170;
     final String initDate = _dateTimeToString(_initDateTime());
 
     model.setDateControllerText(initDate);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 18.0),
-      child: Form(
-        key: BillTo.formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Bill To', style: TextStyle(fontSize: 18, color: purple)),
-            gaph,
-            CustomTextFormField(
-              key: BillTo.clientNameKey,
-              title: "Client's Name",
-              controller: model.clientNameController,
-            ),
-            gaph,
-            CustomTextFormField(
-              key: BillTo.clientEmailKey,
-              title: "Client's Email",
-              controller: model.clientEmailController,
-            ),
-            gaph,
-            CustomTextFormField(
-              key: BillTo.streetAddressKey,
-              title: 'Street Address',
-              controller: model.streetAddController,
-            ),
-            gaph,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: textInputWidth,
-                  child: CustomTextFormField(
-                      key: BillTo.cityKey,
-                      title: 'City',
-                      controller: model.cityController),
-                ),
-                SizedBox(
-                  width: textInputWidth,
-                  child: CustomTextFormField(
-                      key: BillTo.postCodeKey,
-                      title: 'Post Code',
-                      controller: model.postCodeController),
-                ),
-              ],
-            ),
-            gaph,
-            CustomTextFormField(
-                key: BillTo.countryKey,
-                title: 'Country',
-                controller: model.countryController),
-            gaph,
-            CustomTextFormField(
-              key: BillTo.dateKey,
-              title: 'Invoice Date',
-              controller: model.dateController,
-              onTap: () async {
-                DateTime? newDate = await showDatePicker(
-                  context: context,
-                  initialDate: _initDateTime(),
-                  firstDate: DateTime(2023),
-                  lastDate: DateTime(3000),
-                );
-                if (newDate != null) {
-                  model.dateController.text = _dateTimeToString(newDate);
-                }
-              },
-            ),
-            gaph,
-            CustomTextFormField(
-              key: BillTo.projectDescKey,
-              title: 'Project Description',
-              controller: model.projectDescController,
-            ),
-            gaph,
-          ],
-        ),
+    return Form(
+      key: BillTo.formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('Bill To', style: textStyle),
+          gaph,
+          CustomTextFormField(
+            key: BillTo.clientNameKey,
+            title: "Client's Name",
+            controller: model.clientNameController,
+          ),
+          gaph,
+          CustomTextFormField(
+            key: BillTo.clientEmailKey,
+            title: "Client's Email",
+            controller: model.clientEmailController,
+          ),
+          gaph,
+          CustomTextFormField(
+            key: BillTo.streetAddressKey,
+            title: 'Street Address',
+            controller: model.streetAddController,
+          ),
+          gaph,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: textInputWidth,
+                child: CustomTextFormField(
+                    key: BillTo.cityKey,
+                    title: 'City',
+                    controller: model.cityController),
+              ),
+              SizedBox(
+                width: textInputWidth,
+                child: CustomTextFormField(
+                    key: BillTo.postCodeKey,
+                    title: 'Post Code',
+                    controller: model.postCodeController),
+              ),
+            ],
+          ),
+          gaph,
+          CustomTextFormField(
+              key: BillTo.countryKey,
+              title: 'Country',
+              controller: model.countryController),
+          gaph,
+          CustomTextFormField(
+            key: BillTo.dateKey,
+            title: 'Invoice Date',
+            controller: model.dateController,
+            onTap: () async {
+              DateTime? newDate = await showDatePicker(
+                context: context,
+                initialDate: _initDateTime(),
+                firstDate: DateTime(2023),
+                lastDate: DateTime(3000),
+              );
+              if (newDate != null) {
+                model.dateController.text = _dateTimeToString(newDate);
+              }
+            },
+          ),
+          gaph,
+          const CustomDropDownMenu(),
+          gaph,
+          CustomTextFormField(
+            key: BillTo.projectDescKey,
+            title: 'Project Description',
+            controller: model.projectDescController,
+          ),
+          gaph,
+          Text(
+            'Item List',
+            style: textStyle,
+          ),
+          gaph,
+          const CustomListItem()
+        ],
       ),
     );
   }
