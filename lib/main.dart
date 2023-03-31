586992a0825b4dda90f6f2920dd47fd6e242d119
Kwe <Kweayon@gmail.com>
2023-03-31 11:55:11 -0500

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:invoiceapp/constants/router/app_router.dart';
 import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
 import 'package:invoiceapp/src/features/newInvoice/domain/bill_to_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/item_list_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/list_item_model.dart';
 import 'package:invoiceapp/src/features/shared/services/get_it.dart';
 import 'package:provider/provider.dart';
 
@@ -14,6 +16,12 @@ void main() {
       ),
       ChangeNotifierProvider(
         create: _billToNotifier,
+      ),
+      ChangeNotifierProvider(
+        create: _itemListNotifier,
+      ),
+      ChangeNotifierProvider(
+        create: _customListItemModelNotifier,
       )
     ],
     child: _child,
@@ -26,3 +34,8 @@ final _child = MaterialApp.router(
 BillFromModel _billFromNotifier(context) => BillFromModel();
 
 BillToModel _billToNotifier(context) => BillToModel();
+
+ItemListModel _itemListNotifier(context) => ItemListModel();
+
+CustomListItemModel _customListItemModelNotifier(context) =>
+    CustomListItemModel();
