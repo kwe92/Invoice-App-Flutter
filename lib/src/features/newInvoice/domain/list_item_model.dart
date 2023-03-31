2cd2f8db93865745c83e4c3ae1f8d1a5292b7945
Kwe <Kweayon@gmail.com>
2023-03-31 11:56:15 -0500

    Implemented List Item Model

---
+++ lib/src/features/newInvoice/domain/list_item_model.dart
@@ -0,0 +1,39 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/material.dart';
+
+class CustomListItemModel extends ChangeNotifier {
+  TextEditingController _itemNameController = TextEditingController();
+  TextEditingController _quantityController = TextEditingController();
+  TextEditingController _priceController = TextEditingController();
+  TextEditingController _totalController = TextEditingController();
+
+  TextEditingController get itemNameController => _itemNameController;
+  TextEditingController get quantityController => _quantityController;
+  TextEditingController get priceController => _priceController;
+  TextEditingController get totalController => _totalController;
+
+  // _quantityController;
+  // _priceController;
+  // _totalController;
+
+  void clearItemNameController() {
+    _itemNameController.clear();
+    notifyListeners();
+  }
+
+  void clearQuantityController() {
+    _quantityController.clear();
+    notifyListeners();
+  }
+
+  void clearPriceController() {
+    _priceController.clear();
+    notifyListeners();
+  }
+
+  void clearTotalController() {
+    _totalController.clear();
+    notifyListeners();
+  }
+}
