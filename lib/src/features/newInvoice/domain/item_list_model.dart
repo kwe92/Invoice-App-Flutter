6181d08d8abe53e0b5d0e41258bb3cbb4c0faddc
Kwe <Kweayon@gmail.com>
2023-04-02 14:59:29 -0500

    Update

--- lib/src/features/newInvoice/domain/item_list_model.dart
+++ lib/src/features/newInvoice/domain/item_list_model.dart
@@ -1,14 +1,24 @@
 // ignore_for_file: prefer_final_fields
 
 import 'package:flutter/material.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/list_item_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/presentation/list_item.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/list_item_stateful.dart';
 
 class ItemListModel extends ChangeNotifier {
-  final Map<int, CustomListItemStateful> _items = {};
-  Map<int, CustomListItemStateful> get items => _items;
+  final Map<int, CustomListItem> _items = {};
+  final Map<int, CustomListItemModel> _itemModels = {};
 
-  void addItem(Map<int, CustomListItemStateful> entry) {
+  Map<int, CustomListItem> get items => _items;
+  Map<int, CustomListItemModel> get itemModels => _itemModels;
+
+  void addItem(Map<int, CustomListItem> entry) {
     _items.addAll(entry);
+    notifyListeners();
+  }
+
+  void addItemModel(Map<int, CustomListItemModel> entry) {
+    _itemModels.addAll(entry);
     notifyListeners();
   }
 
