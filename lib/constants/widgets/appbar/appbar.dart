f72ea44e03a47a3928806cc9e723c5aebefc0ff2
Kwe <Kweayon@gmail.com>
2023-04-05 12:21:11 -0500

    Implemented click event to ProfileScreen

--- lib/constants/widgets/appbar/appbar.dart
+++ lib/constants/widgets/appbar/appbar.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:invoiceapp/constants/widgets/appbar/app_icon.dart';
+import 'package:invoiceapp/src/features/authentication/presentation/profile_screen.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
 
@@ -16,41 +18,46 @@ class CustomAppBar extends StatelessWidget implements PreferredSize {
   Widget build(BuildContext context) => PreferredSize(
         preferredSize: CustomTheme.appBarSize,
         child: AppBar(
-          backgroundColor: CustomTheme.darkColors['shade3'],
-          leading: AppIcon(radius: _radius),
-          // actions: _otherIcons(),
-          actions: _OtherIcons.otherIcons,
-        ),
+            backgroundColor: CustomTheme.darkColors['shade3'],
+            leading: AppIcon(radius: _radius),
+            // actions: _otherIcons(),
+            actions:
+                // _OtherIcons.otherIcons(context),
+                [
+              Row(
+                children: [
+                  SvgPicture.asset('assets/icon-moon.svg'),
+                  Gaps.width(21),
+                  Container(
+                    height: 56,
+                    width: 1,
+                    color: CustomTheme.lightColors['shade0'],
+                  ),
+                  Gaps.width(21),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute<void>(
+                            builder: (BuildContext context) =>
+                                const CustomProfileScreen(),
+                          ));
+                    },
+                    child: const SizedBox(
+                      height: 28,
+                      width: 28,
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('assets/image-avatar.jpg'),
+                      ),
+                    ),
+                  ),
+                  Gaps.width(21)
+                ],
+              )
+            ]),
       );
 
   @override
   // TODO: Is this correct code?
   Widget get child => const SizedBox();
-}
-
-class _OtherIcons {
-  const _OtherIcons();
-
-  static final otherIcons = [
-    Row(
-      children: [
-        SvgPicture.asset('assets/icon-moon.svg'),
-        Gaps.width(21),
-        Container(
-          height: 56,
-          width: 1,
-          color: CustomTheme.lightColors['shade0'],
-        ),
-        Gaps.width(21),
-        const SizedBox(
-          height: 28,
-          width: 28,
-          child: CircleAvatar(
-            backgroundImage: AssetImage('assets/image-avatar.jpg'),
-          ),
-        ),
-        Gaps.width(21)
-      ],
-    )
-  ];
 }
