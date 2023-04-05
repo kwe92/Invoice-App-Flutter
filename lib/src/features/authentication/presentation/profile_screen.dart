d4363048eec5098f69ecdebbbf11c64584dd71b9
Kwe <Kweayon@gmail.com>
2023-04-05 16:31:35 -0500

    Update

--- lib/src/features/authentication/presentation/profile_screen.dart
+++ lib/src/features/authentication/presentation/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:invoiceapp/theme/theme.dart';
 
 class CustomProfileScreen extends StatelessWidget {
   static final _providers = [EmailAuthProvider()];
@@ -10,7 +11,10 @@ class CustomProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) => SafeArea(
         child: Scaffold(
-          appBar: AppBar(title: const Text('Profile Screen')),
+          appBar: AppBar(
+            title: const Text('Profile Screen'),
+            backgroundColor: CustomTheme.darkColors['shade3'],
+          ),
           body: ProfileScreen(
             providers: _providers,
             actions: [
