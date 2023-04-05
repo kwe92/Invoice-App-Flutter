471121de817f580f90832b4c7cb742ca07ee8577
Kwe <Kweayon@gmail.com>
2023-04-05 16:31:14 -0500

    Implemented Firebase Helper Class

---
+++ lib/constants/firebase/app_firebase.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AppFirebase {
+  const AppFirebase();
+
+  static Future<void> loadData(
+          String collection, Map<String, Object> data) async =>
+      await FirebaseFirestore.instance.collection('invoices').doc().set(data);
+  static String getCurrentUserId() => FirebaseAuth.instance.currentUser!.uid;
+}
