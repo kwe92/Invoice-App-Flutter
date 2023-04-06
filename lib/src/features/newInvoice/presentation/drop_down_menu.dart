227f8a4bda9cfc1ddbbf4e572250fc9733a33269
Kwe <Kweayon@gmail.com>
2023-04-06 12:54:36 -0500

    Update

--- lib/src/features/newInvoice/presentation/drop_down_menu.dart
+++ lib/src/features/newInvoice/presentation/drop_down_menu.dart
@@ -28,7 +28,7 @@ class CustomDropDownMenu extends StatelessWidget {
                 focusedBorder: _border(purple),
               ),
               dropdownMenuEntries: <String>[
-                'Net 1` Day',
+                'Net 1 Day',
                 'Net 7 Days',
                 'Net 14 Days',
                 'Net 30 Days'
