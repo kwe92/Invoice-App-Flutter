9462737e1ba605269011ccfe2a231600c846f2fd
Kwe <Kweayon@gmail.com>
2023-04-07 18:19:50 -0500

    Update

--- lib/constants/router/app_router.dart
+++ lib/constants/router/app_router.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:invoiceapp/constants/widgets/base_scaffold/base_scaffold.dart';
 import 'package:invoiceapp/src/features/authentication/presentation/profile_screen.dart';
 import 'package:invoiceapp/src/features/authentication/presentation/sign_in_screen.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_screen.dart';
 import 'package:invoiceapp/src/features/newInvoice/presentation/new_invoice.dart';
+import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
 import 'package:invoiceapp/src/features/shared/services/get_it.dart';
+import 'package:invoiceapp/theme/source_of_truth.dart';
 
 AppRouter get appRouter {
   return getIt.get<AppRouter>();
@@ -21,7 +24,30 @@ class AppRouter {
         path: 'newInvoice',
         builder: _newInvoiceScreen,
       ),
-      GoRoute(path: 'profile', builder: _profileScreen)
+      GoRoute(path: 'profile', builder: _profileScreen),
+      // TODO: Create edit Invoice screen
+      GoRoute(
+          path: 'editInvoice',
+          builder: (context, state) {
+            InvoiceFormModel invoice = state.extra as InvoiceFormModel;
+            print('From app_router: ${invoice.billToText}');
+            return BaseScaffold(
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Edit Invoice Page'),
+                    Gaps.gaph12,
+                    ElevatedButton(
+                        onPressed: () {
+                          context.pop();
+                        },
+                        child: const Text('Back'))
+                  ],
+                ),
+              ),
+            );
+          })
     ]),
   ]);
 }
