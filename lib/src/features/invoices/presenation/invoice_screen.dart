0b72d2effb01c6598e1fc9d8e4681fee01cdb757
Kwe <Kweayon@gmail.com>
2023-04-07 13:18:47 -0500

    Update

--- lib/src/features/invoices/presenation/invoice_screen.dart
+++ lib/src/features/invoices/presenation/invoice_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/constants/widgets/base_scaffold/base_scaffold.dart';
 import 'package:invoiceapp/src/features/invoices/data/invoice.dart';
+import 'package:invoiceapp/src/features/invoices/domain/invoices_model.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/invoice_list_tile.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/title_icon_list_tile.dart';
+import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
+import 'package:provider/provider.dart';
 
 class InvoiceScreen extends StatelessWidget {
   static const baseScaffoldKey = Key('baseScaffoldKey');
@@ -24,12 +27,18 @@ class _InvoicesListView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 24.0),
-      child: ListView(key: InvoiceScreen.listViewKey, children: <Widget>[
-        const TitleIconListTile(
-          key: InvoiceScreen.titleIconListTileKey,
-        ),
-        for (int i = 0; i < 12; i++) InvoiceListTile(invoice: _invoice),
-      ]),
+      child: Consumer(
+        builder: (BuildContext context, InvoicesModel model, _) {
+          // model.notifyListeners();
+          return ListView(key: InvoiceScreen.listViewKey, children: <Widget>[
+            const TitleIconListTile(
+              key: InvoiceScreen.titleIconListTileKey,
+            ),
+            for (final InvoiceFormModel invoice in model.invoices)
+              InvoiceListTile(invoice: invoice),
+          ]);
+        },
+      ),
     );
   }
 }
