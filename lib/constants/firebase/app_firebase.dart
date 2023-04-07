ab188e022cb78aa1dca5494431fd718939f729fa
Kwe <Kweayon@gmail.com>
2023-04-06 23:25:56 -0500

    Update

--- lib/constants/firebase/app_firebase.dart
+++ lib/constants/firebase/app_firebase.dart
@@ -7,5 +7,6 @@ class AppFirebase {
   static Future<void> loadData(
           String collection, Map<String, Object> data) async =>
       await FirebaseFirestore.instance.collection('invoices').doc().set(data);
+
   static String getCurrentUserId() => FirebaseAuth.instance.currentUser!.uid;
 }
