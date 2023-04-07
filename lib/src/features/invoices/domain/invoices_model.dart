c9bc6408a7db610f04587393a7ba6192b8d4c5b2
Kwe <Kweayon@gmail.com>
2023-04-07 18:19:41 -0500

    Implemented Map for O(1) Constant time complexity

--- lib/src/features/invoices/domain/invoices_model.dart
+++ lib/src/features/invoices/domain/invoices_model.dart
@@ -7,9 +7,11 @@ import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
 class InvoicesModel extends ChangeNotifier {
   // TODO: Figure out how to make it a Map
   // Map<String, Object> _invoices = {};
-  List _invoices = [];
+  // List _invoices = [];
+  Map _invoices = {};
 
-  List get invoices => _invoices;
+  // List get invoices => _invoices;
+  Map get invoices => _invoices;
 
   InvoicesModel() {
     _init();
@@ -24,12 +26,11 @@ class InvoicesModel extends ChangeNotifier {
   }
 
   void _invoicesCallback(QuerySnapshot snapshot) {
-    _invoices = [
-      for (final document in snapshot.docs)
-
-        // print(document.data())
-        InvoiceFormModel.fromJSON(document.data() as Map<String, dynamic>)
-    ];
+    _invoices = {
+      for (final invoice in snapshot.docs as List<QueryDocumentSnapshot<Map?>>)
+        invoice.data()!['invoiceId']:
+            InvoiceFormModel.fromJSON(invoice.data() as Map<String, dynamic>)
+    };
 
     notifyListeners();
   }
