efdae1ff5a3ff10b75cc4a75318e79d4a2c7db65
Kwe <Kweayon@gmail.com>
2023-04-09 13:50:05 -0500

    Shared Widget

---
+++ lib/src/features/shared/widgets/custom_text_form_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:invoiceapp/theme/theme.dart';
+
+typedef VoidCallback = Function();
+
+class CustomTextFormField extends StatelessWidget {
+  final String title;
+  final TextEditingController controller;
+
+  final VoidCallback? onTap;
+
+  const CustomTextFormField(
+      {required this.title, required this.controller, this.onTap, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final purple = CustomTheme.otherColors['purple0'] as Color;
+    final grey = CustomTheme.lightColors['shade1'] as Color;
+    return TextFormField(
+      onTap: onTap,
+      controller: controller,
+      decoration: InputDecoration(
+        focusColor: purple,
+        label: Text(
+          title,
+          style: TextStyle(color: grey),
+        ),
+        border: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: CustomTheme.lightColors['shade1'] as Color),
+            borderRadius: BorderRadius.circular(6)),
+        focusedBorder:
+            OutlineInputBorder(borderSide: BorderSide(color: purple)),
+      ),
+    );
+  }
+}
