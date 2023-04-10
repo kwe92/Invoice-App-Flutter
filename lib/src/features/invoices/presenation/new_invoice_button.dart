ff76b1b87db37ab873490f27dd8038ffc496f8c4
Kwe <Kweayon@gmail.com>
2023-04-10 13:54:26 -0500

    Implemented Clear Controllers

--- lib/src/features/invoices/presenation/new_invoice_button.dart
+++ lib/src/features/invoices/presenation/new_invoice_button.dart
@@ -1,60 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/bill_to_model.dart';
+import 'package:invoiceapp/src/features/newInvoice/domain/item_list_model.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
+import 'package:provider/provider.dart';
 
 class NewInvoiceButton extends StatelessWidget {
   const NewInvoiceButton({super.key});
 
   @override
-  Widget build(BuildContext context) => GestureDetector(
-        onTap: () => context.push('/newInvoice'),
-        child: Container(
-          width: 118,
-          height: 58,
-          decoration: BoxDecoration(
-            color: CustomTheme.otherColors['purple0'],
-            borderRadius: BorderRadius.circular(50),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  alignment: Alignment.center,
-                  width: 38,
-                  height: 38,
-                  child: SizedBox(
-                    width: 38 / 2.25,
-                    height: 38 / 2.25,
-                    child: SvgPicture.asset(
-                      'assets/icon-plus.svg',
-                      // colorFilter: ColorFilter.mode(
-                      //     CustomTheme.otherColors['purple0'] as Color,
-                      //     BlendMode.srcIn),
+  Widget build(BuildContext context) => Consumer3(builder: (context,
+          BillFromModel billFromModel,
+          BillToModel billToModel,
+          ItemListModel itemsModel,
+          child) {
+        return GestureDetector(
+          onTap: () {
+            context.push('/newInvoice');
+            billFromModel.clearAllControllers();
+            billToModel.clearAllControllers();
+            itemsModel.clearItemsState();
+          },
+          child: Container(
+            width: 118,
+            height: 58,
+            decoration: BoxDecoration(
+              color: CustomTheme.otherColors['purple0'],
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    alignment: Alignment.center,
+                    width: 38,
+                    height: 38,
+                    child: SizedBox(
+                      width: 38 / 2.25,
+                      height: 38 / 2.25,
+                      child: SvgPicture.asset(
+                        'assets/icon-plus.svg',
+                        // colorFilter: ColorFilter.mode(
+                        //     CustomTheme.otherColors['purple0'] as Color,
+                        //     BlendMode.srcIn),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Gaps.gapw8,
-              Expanded(
-                child: Text(
-                  'New',
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: CustomTheme.lightColors['shade3'],
-                    fontWeight: FontWeight.w400,
+                Gaps.gapw8,
+                Expanded(
+                  child: Text(
+                    'New',
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: CustomTheme.lightColors['shade3'],
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-      );
+        );
+      });
 }
