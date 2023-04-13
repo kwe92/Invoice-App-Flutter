999d09d46c5016f2e464aedd801b25e27263495c
Kwe <Kweayon@gmail.com>
2023-04-12 23:43:29 -0500

    Update

---
+++ lib/src/features/shared/widgets/invoice_form_screen/domain/item_list_model.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/material.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/list_item_model.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/presentation/list_item.dart';
+
+class ItemListModel extends ChangeNotifier {
+  Map<String, CustomListItem> _items = {};
+  Map<String, CustomListItemModel> _itemModels = {};
+
+  Map<String, CustomListItem> get items => _items;
+  Map<String, CustomListItemModel> get itemModels => _itemModels;
+
+  void addItem(Map<String, CustomListItem> entry) {
+    _items.addAll(entry);
+    notifyListeners();
+  }
+
+  void addItemModel(Map<String, CustomListItemModel> entry) {
+    _itemModels.addAll(entry);
+    notifyListeners();
+  }
+
+  void removeItem(String index) {
+    _items.remove(index);
+    notifyListeners();
+    _removeItemModel(index);
+  }
+
+  void clearItemsState() {
+    _itemModels = {};
+    _items = {};
+
+    notifyListeners();
+  }
+
+  void _removeItemModel(String index) {
+    _itemModels.remove(index);
+    notifyListeners();
+  }
+}
---
+++ lib/src/features/shared/widgets/invoice_form_screen/domain/list_item_model.dart
@@ -0,0 +1,57 @@
+// ignore_for_file: prefer_final_fields
+
+import 'package:flutter/material.dart';
+
+class CustomListItemModel extends ChangeNotifier {
+  TextEditingController _itemNameController = TextEditingController();
+  TextEditingController _quantityController = TextEditingController();
+  TextEditingController _priceController = TextEditingController();
+  TextEditingController _totalController = TextEditingController();
+
+  TextEditingController get itemNameController => _itemNameController;
+  TextEditingController get quantityController => _quantityController;
+  TextEditingController get priceController => _priceController;
+  TextEditingController get totalController => _totalController;
+
+  Map<String, String> get allControllerText {
+    return {
+      'itemName': itemNameController.text,
+      'quantity': quantityController.text,
+      'price': priceController.text,
+      'total': totalController.text,
+    };
+  }
+
+  void clearItemNameController() {
+    _itemNameController.clear();
+    notifyListeners();
+  }
+
+  void clearQuantityController() {
+    _quantityController.clear();
+    notifyListeners();
+  }
+
+  void clearPriceController() {
+    _priceController.clear();
+    notifyListeners();
+  }
+
+  void clearTotalController() {
+    _totalController.clear();
+    notifyListeners();
+  }
+
+  void loadControllers(Map<String, dynamic> item) {
+    print('ITEM FROM loadControllers:$item');
+    print('ITEM: FROM loadControllers:${item["price"]}');
+
+    print('Item keys loadControllers: ${item.keys}');
+    quantityController.text = item['quantity'] ?? '';
+    itemNameController.text = item['itemName'] ?? '';
+    // TODO: Fix the price, it is not displaying even though there is data
+    priceController.text = item['price'] ?? '';
+    totalController.text = item['total'] ?? '';
+    notifyListeners();
+  }
+}
