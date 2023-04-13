364e4a84473b70e38b034238a81a435ebe570eaa
Kwe <Kweayon@gmail.com>
2023-04-12 23:44:28 -0500

    Updates

--- test/robots/invoice_screen_robot.dart
+++ test/robots/invoice_screen_robot.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_screen.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/title_icon_list_tile.dart';
-import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
-import 'package:invoiceapp/src/features/newInvoice/presentation/new_invoice.dart';
-import 'package:provider/provider.dart';
 
 class InvoiceScreenRobot {
   final WidgetTester tester;
