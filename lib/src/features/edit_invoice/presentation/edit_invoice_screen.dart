5a2230e79481e780f7b8c421caad4e5d36dcd039
Kwe <Kweayon@gmail.com>
2023-04-13 16:31:54 -0500

    Update

--- lib/src/features/edit_invoice/presentation/edit_invoice_screen.dart
+++ lib/src/features/edit_invoice/presentation/edit_invoice_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:invoiceapp/src/features/new_invoice_screen/presentation/submit_button.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_from_model.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_to_model.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/item_list_model.dart';
 import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/presentation/invoice_form_screen.dart';
+import 'package:provider/provider.dart';
 
 // TODO: Make more modular | New Invoice && Edit invoice Screen should be mostly modular | buttons at the bottom may differ
 
@@ -8,7 +13,16 @@ class EditInvoiceScreen extends StatelessWidget {
   const EditInvoiceScreen({required this.docId, super.key});
   @override
   Widget build(BuildContext context) {
-    print('FROM EDIT INVOICE SCREEN: $docId');
-    return const InvoiceFormScreen();
+    // print('FROM EDIT INVOICE SCREEN: $docId');
+    return Consumer3(
+        builder: (context, BillFromModel billFromModel, BillToModel billToModel,
+                ItemListModel itemsModel, child) =>
+            InvoiceFormScreen(
+              button: SubmitButton(
+                  firebaseId: docId,
+                  billFromModel: billFromModel,
+                  billToModel: billToModel,
+                  itemsModel: itemsModel),
+            ));
   }
 }
