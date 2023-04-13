ae585bfb9340f909867a4e16d788dd985f5f1e81
Kwe <Kweayon@gmail.com>
2023-04-12 23:42:33 -0500

    Refactor

--- lib/src/features/invoices/presenation/new_invoice_button.dart
+++ lib/src/features/invoices/presenation/new_invoice_button.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
-import 'package:invoiceapp/src/features/newInvoice/domain/bill_to_model.dart';
-import 'package:invoiceapp/src/features/newInvoice/domain/item_list_model.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_from_model.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_to_model.dart';
+import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/item_list_model.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
 import 'package:provider/provider.dart';
