ccfed96a8fb95651f7c0ceb67c4135832f0c8ab8
Kwe <Kweayon@gmail.com>
2023-04-15 14:12:14 -0500

    Enhancement-Created enum HashKeys for commonly used hash table keys

--- lib/constants/enums/status.dart
+++ lib/constants/enums/status.dart
@@ -1,1 +1,14 @@
 enum InvoiceStatus { paid, pending, draft }
+
+enum HashKeys {
+  invoiceId,
+  userId,
+  docId,
+  createdAt,
+  paymentDue,
+  status,
+  billFromText,
+  billToText,
+  listItems,
+  total
+}
