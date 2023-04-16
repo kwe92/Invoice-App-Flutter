0a0ea1594e9f2dda967284884744061ca9a6d452
Kwe <Kweayon@gmail.com>
2023-04-15 22:34:28 -0500

    Refactor-Code Clean Up

--- lib/constants/enums/status.dart
+++ lib/constants/enums/status.dart
@@ -1,14 +1,1 @@
 enum InvoiceStatus { paid, pending, draft }
-
-enum HashKeys {
-  invoiceId,
-  userId,
-  docId,
-  createdAt,
-  paymentDue,
-  status,
-  billFromText,
-  billToText,
-  listItems,
-  total
-}
