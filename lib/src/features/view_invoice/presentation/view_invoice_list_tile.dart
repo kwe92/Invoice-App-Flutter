dc4dea7f02ba5e4c24294476894c61eee8a9384a
Kwe <Kweayon@gmail.com>
2023-04-18 12:57:41 -0500

    Styled Invoice ID

--- lib/src/features/view_invoice/presentation/view_invoice_list_tile.dart
+++ lib/src/features/view_invoice/presentation/view_invoice_list_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/list_tile_decoration.dart';
 import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
+import 'package:invoiceapp/src/features/view_invoice/presentation/description_section/custom_text_style.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 
 class ViewInvoiceListTile extends StatelessWidget {
@@ -26,7 +27,10 @@ class ViewInvoiceListTile extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text('Invoice ${invoice.invoiceId}'),
+                Text(
+                  'Invoice ${invoice.invoiceId}',
+                  style: const CustomTextStyle(),
+                ),
                 Gaps.gapw8,
                 _Capsule(
                   child: Center(
