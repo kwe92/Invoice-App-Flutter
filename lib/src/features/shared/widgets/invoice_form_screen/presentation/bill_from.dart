041f87ab0c39080baeafcd54be3325dc1195d4b9
Kwe <Kweayon@gmail.com>
2023-04-24 15:52:38 -0500

    Added formState property

--- lib/src/features/shared/widgets/invoice_form_screen/presentation/bill_from.dart
+++ lib/src/features/shared/widgets/invoice_form_screen/presentation/bill_from.dart
@@ -6,17 +6,12 @@ import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
 
 class BillFrom extends StatelessWidget {
-  static const formKey = Key('formKey');
-  static const streetAddressKey = Key('streetAddressKey');
-  static const cityKey = Key('cityKey');
-  static const postCodeKey = Key('postCodeKey');
-  static const countryKey = Key('countryKey');
-  static const clearTextKey = Key('clearTextKey');
-
   final BillFromModel model;
   final InvoiceFormModel? invoice;
+  final GlobalKey<FormState> formState;
 
-  const BillFrom({required this.model, this.invoice, super.key});
+  const BillFrom(
+      {required this.model, required this.formState, this.invoice, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,16 +22,21 @@ class BillFrom extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 18.0),
       child: Form(
-        key: BillFrom.formKey,
+        key: formState,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text('Bill From', style: TextStyle(fontSize: 18, color: purple)),
+            Text(
+              'Bill From',
+              style: TextStyle(fontSize: 18, color: purple),
+            ),
             gaph,
             CustomTextFormField(
-              key: BillFrom.streetAddressKey,
               title: 'Street Address',
               controller: model.streetAddController,
+              validator: (s) {
+                if (s == null || s.isEmpty) return 'Please enter a value';
+              },
             ),
             gaph,
             Row(
@@ -45,24 +45,18 @@ class BillFrom extends StatelessWidget {
                 SizedBox(
                   width: textInputWidth,
                   child: CustomTextFormField(
-                      key: BillFrom.cityKey,
-                      title: 'City',
-                      controller: model.cityController),
+                      title: 'City', controller: model.cityController),
                 ),
                 SizedBox(
                   width: textInputWidth,
                   child: CustomTextFormField(
-                      key: BillFrom.postCodeKey,
-                      title: 'Post Code',
-                      controller: model.postCodeController),
+                      title: 'Post Code', controller: model.postCodeController),
                 ),
               ],
             ),
             gaph,
             CustomTextFormField(
-                key: BillFrom.countryKey,
-                title: 'Country',
-                controller: model.countryController),
+                title: 'Country', controller: model.countryController),
             gaph,
           ],
         ),
@@ -70,3 +64,11 @@ class BillFrom extends StatelessWidget {
     );
   }
 }
+
+
+  // static const formKey = Key('formKey');
+  // static const streetAddressKey = Key('streetAddressKey');
+  // static const cityKey = Key('cityKey');
+  // static const postCodeKey = Key('postCodeKey');
+  // static const countryKey = Key('countryKey');
+  // static const clearTextKey = Key('clearTextKey');
