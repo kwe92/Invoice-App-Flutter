45125b45cffa2d8680b391e8e288a8c3b04dbac6
Kwe <Kweayon@gmail.com>
2023-04-25 07:45:48 -0500

    Commented out code

--- test/robots/robot.dart
+++ test/robots/robot.dart
@@ -1,63 +1,63 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:invoiceapp/constants/router/app_router.dart';
-// import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
-import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_from_model.dart';
-import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_to_model.dart';
-import 'package:provider/provider.dart';
-import 'invoice_screen_robot.dart';
-import 'new_invoice_screen_robot.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:invoiceapp/constants/router/app_router.dart';
+// // import 'package:invoiceapp/src/features/newInvoice/domain/bill_from_model.dart';
+// import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_from_model.dart';
+// import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_to_model.dart';
+// import 'package:provider/provider.dart';
+// import 'invoice_screen_robot.dart';
+// import 'new_invoice_screen_robot.dart';
 
-class Robot {
-  final WidgetTester tester;
-  final InvoiceScreenRobot invoice;
-  final NewInvoiceScreenRobot newInvoice;
+// class Robot {
+//   final WidgetTester tester;
+//   final InvoiceScreenRobot invoice;
+//   final NewInvoiceScreenRobot newInvoice;
 
-  Robot({required this.tester})
-      : invoice = InvoiceScreenRobot(tester: tester),
-        newInvoice = NewInvoiceScreenRobot(tester: tester);
+//   Robot({required this.tester})
+//       : invoice = InvoiceScreenRobot(tester: tester),
+//         newInvoice = NewInvoiceScreenRobot(tester: tester);
 
-  Future<void> pumpApp() async =>
-      await tester.pumpWidget(_invoiceScreen(appRouter.router));
+//   Future<void> pumpApp() async =>
+//       await tester.pumpWidget(_invoiceScreen(appRouter.router));
 
-  Future<void> pumpWidget(Widget widget) async =>
-      await tester.pumpWidget(MultiProvider(
-        providers: [
-          ChangeNotifierProvider<BillFromModel>(
-            create: _billFromNotifier,
-          ),
-          ChangeNotifierProvider<BillToModel>(
-            create: _billToNotifier,
-          )
-        ],
-        child: MaterialApp(home: widget),
-      ));
-}
+//   Future<void> pumpWidget(Widget widget) async =>
+//       await tester.pumpWidget(MultiProvider(
+//         providers: [
+//           ChangeNotifierProvider<BillFromModel>(
+//             create: _billFromNotifier,
+//           ),
+//           ChangeNotifierProvider<BillToModel>(
+//             create: _billToNotifier,
+//           )
+//         ],
+//         child: MaterialApp(home: widget),
+//       ));
+// }
 
-Widget _invoiceScreen(GoRouter router) => MultiProvider(
-      providers: [
-        ChangeNotifierProvider<BillFromModel>(
-          create: _billFromNotifier,
-        ),
-        ChangeNotifierProvider<BillToModel>(
-          create: _billToNotifier,
-        )
-      ],
-      child: MaterialApp.router(
-        routeInformationParser: router.routeInformationParser,
-        routerDelegate: router.routerDelegate,
-      ),
-    );
-
-BillFromModel _billFromNotifier(context) => BillFromModel();
-
-BillToModel _billToNotifier(context) => BillToModel();
-
-// Widget _invoiceScreen(GoRouter router) => Provider<BillFromModel>(
-//       create: (BuildContext context) => BillFromModel(),
+// Widget _invoiceScreen(GoRouter router) => MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider<BillFromModel>(
+//           create: _billFromNotifier,
+//         ),
+//         ChangeNotifierProvider<BillToModel>(
+//           create: _billToNotifier,
+//         )
+//       ],
 //       child: MaterialApp.router(
 //         routeInformationParser: router.routeInformationParser,
 //         routerDelegate: router.routerDelegate,
 //       ),
 //     );
+
+// BillFromModel _billFromNotifier(context) => BillFromModel();
+
+// BillToModel _billToNotifier(context) => BillToModel();
+
+// // Widget _invoiceScreen(GoRouter router) => Provider<BillFromModel>(
+// //       create: (BuildContext context) => BillFromModel(),
+// //       child: MaterialApp.router(
+// //         routeInformationParser: router.routeInformationParser,
+// //         routerDelegate: router.routerDelegate,
+// //       ),
+// //     );
--- test/src/features/invoices/presenation/invoice_screen_test.dart
+++ test/src/features/invoices/presenation/invoice_screen_test.dart
@@ -1,49 +1,49 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:invoiceapp/src/features/shared/services/get_it.dart';
-import '../../../../robots/robot.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:invoiceapp/src/features/shared/services/get_it.dart';
+// import '../../../../robots/robot.dart';
 
-void main() {
-  setUp(() => configureDependencies());
-  group('Widget Test', () {
-    testWidgets('''
-      Given: app loads
-      When: tap on new invoice button
-      Then: navigate to the new invoice screen
-      ''', (WidgetTester tester) async {
-      // TODO: Why is this bad and how to create a mock
-      final r = Robot(tester: tester);
+// void main() {
+//   setUp(() => configureDependencies());
+//   group('Widget Test', () {
+//     testWidgets('''
+//       Given: app loads
+//       When: tap on new invoice button
+//       Then: navigate to the new invoice screen
+//       ''', (WidgetTester tester) async {
+//       // TODO: Why is this bad and how to create a mock
+//       final r = Robot(tester: tester);
 
-      await r.pumpApp();
+//       await r.pumpApp();
 
-      r.invoice.findInvoiceScreenBaseLayout();
+//       r.invoice.findInvoiceScreenBaseLayout();
 
-      await r.invoice.tapNewInvoiceButton();
+//       await r.invoice.tapNewInvoiceButton();
 
-      // await r.pumpWidget(const NewInvoice());
+//       // await r.pumpWidget(const NewInvoice());
 
-      r.newInvoice.findBillFrom();
-    });
+//       r.newInvoice.findBillFrom();
+//     });
 
-    testWidgets('''
-      Given: new invoice screen loads 
-      When: input is entered && clear buttons pressed && back button pressed
-      Then: text is entered && text is cleared && back button is pressed
-      ''', (WidgetTester tester) async {
-      final r = Robot(tester: tester);
+//     testWidgets('''
+//       Given: new invoice screen loads 
+//       When: input is entered && clear buttons pressed && back button pressed
+//       Then: text is entered && text is cleared && back button is pressed
+//       ''', (WidgetTester tester) async {
+//       final r = Robot(tester: tester);
 
-      await r.pumpApp();
+//       await r.pumpApp();
 
-      await r.invoice.tapNewInvoiceButton();
+//       await r.invoice.tapNewInvoiceButton();
 
-      await r.newInvoice.enterBillFromText();
+//       await r.newInvoice.enterBillFromText();
 
-      // await r.newInvoice.tapClearText();
+//       // await r.newInvoice.tapClearText();
 
-      // await r.newInvoice.enterBillFromText();
+//       // await r.newInvoice.enterBillFromText();
 
-      // await r.newInvoice.clearIndividualInput();
+//       // await r.newInvoice.clearIndividualInput();
 
-      // await r.newInvoice.tapBackButton();
-    });
-  });
-}
+//       // await r.newInvoice.tapBackButton();
+//     });
+//   });
+// }
