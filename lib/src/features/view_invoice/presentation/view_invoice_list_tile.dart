9b294eccaec247859fe0726baf4502923a64d77a
Kwe <Kweayon@gmail.com>
2023-04-26 15:59:56 -0500

    Added onLongPress variable and modal

--- lib/src/features/view_invoice/presentation/view_invoice_list_tile.dart
+++ lib/src/features/view_invoice/presentation/view_invoice_list_tile.dart
@@ -6,39 +6,44 @@ import 'package:invoiceapp/theme/source_of_truth.dart';
 
 class ViewInvoiceListTile extends StatelessWidget {
   final InvoiceFormModel invoice;
+  final VoidCallback onLongPress;
 
-  const ViewInvoiceListTile({required this.invoice, super.key});
+  const ViewInvoiceListTile(
+      {required this.invoice, required this.onLongPress, super.key});
 
   @override
-  Widget build(BuildContext context) => Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Gaps.gaph6,
-          Container(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            decoration: CustomDecoration.decoration(),
-            height: 75,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'Invoice ${invoice.invoiceId}',
-                  style: const CustomTextStyle(),
-                ),
-                Gaps.gapw8,
-                _Capsule(
-                  child: Center(
-                    child: Text(
-                      invoice.status,
-                      style: const TextStyle(color: Colors.white),
-                    ),
+  Widget build(BuildContext context) => GestureDetector(
+        onLongPress: onLongPress,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Gaps.gaph6,
+            Container(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              decoration: CustomDecoration.decoration(),
+              height: 75,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Invoice ${invoice.invoiceId}',
+                    style: const CustomTextStyle(),
                   ),
-                )
-              ],
+                  Gaps.gapw8,
+                  _Capsule(
+                    child: Center(
+                      child: Text(
+                        invoice.status,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
 }
 
