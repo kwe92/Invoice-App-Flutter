0769b44d7c4341492a92e149dc5ec278aac650af
Kwe <Kweayon@gmail.com>
2023-04-26 19:42:14 -0500

    Refactored if statments to use ternary operators

--- lib/constants/utils/form_validators.dart
+++ lib/constants/utils/form_validators.dart
@@ -1,11 +1,7 @@
 // Form Field Validators
 
-// TODO: Expand RegEx's
-// TODO: Fix _textPattern, its not working correctly. allows user to still inter text with numbers
-// ?? Need to add validators: date, email, payment term
 class FormValidators {
   static const _emptyText = 'Can not be empty.';
-
   static const _textNumPattern = r'^[a-zA-Z0-9 ]*$';
   static const _textPattern = r'^[a-zA-Z]*$';
   static const _naturalNumbersPattern = r'^[0-9]+-[0-9]*$';
@@ -18,56 +14,33 @@ class FormValidators {
   static final RegExp _emailRegex = RegExp(_emailPattern);
   static final RegExp _dateRegex = RegExp(_datePattern);
 
-  static String? textNumField(String? s) {
-    if (s != null) {
-      print('${_textNumRegex.hasMatch(s)}');
-    }
-    if (s == null || s.isEmpty) {
-      return _emptyText;
-    }
-    if (!_textNumRegex.hasMatch(s)) {
-      return 'Letters A-Z and numbers 0-9';
-    }
-    return null;
-  }
+  static String? textNumField(String? s) => s == null || s.isEmpty
+      ? _emptyText
+      : !_textNumRegex.hasMatch(s)
+          ? 'Letters A-Z and numbers 0-9'
+          : null;
 
-  static String? textField(String? s) {
-    if (s == null || s.isEmpty) {
-      return _emptyText;
-    }
-    if (!_textRegex.hasMatch(s)) {
-      return 'Letters A-Z, no spaces.';
-    }
-    return null;
-  }
+  static String? textField(String? s) => s == null || s.isEmpty
+      ? _emptyText
+      : !_textRegex.hasMatch(s)
+          ? 'Letters A-Z, no spaces.'
+          : null;
 
-  static String? numberField(String? s) {
-    if (s == null || s.isEmpty) {
-      return _emptyText;
-    }
-    if (!_numbersRegex.hasMatch(s)) {
-      return 'Numbers 0 - 9, no spaces.';
-    }
-    return null;
-  }
+  static String? numberField(String? s) => s == null || s.isEmpty
+      ? _emptyText
+      : !_numbersRegex.hasMatch(s)
+          ? 'Numbers 0 - 9, no spaces.'
+          : null;
 
-  static String? emailField(String? s) {
-    if (s == null || s.isEmpty) {
-      return _emptyText;
-    }
-    if (!_emailRegex.hasMatch(s)) {
-      return 'Invalid email address.';
-    }
-    return null;
-  }
+  static String? emailField(String? s) => s == null || s.isEmpty
+      ? _emptyText
+      : !_emailRegex.hasMatch(s)
+          ? 'Invalid email address.'
+          : null;
 
-  static String? dateField(String? s) {
-    if (s == null || s.isEmpty) {
-      return _emptyText;
-    }
-    if (!_dateRegex.hasMatch(s)) {
-      return 'Invalid date.';
-    }
-    return null;
-  }
+  static String? dateField(String? s) => s == null || s.isEmpty
+      ? _emptyText
+      : !_dateRegex.hasMatch(s)
+          ? 'Invalid date'
+          : null;
 }
