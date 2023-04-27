3149126aa537e04ce797647edc13151b7c4374de
Kwe <Kweayon@gmail.com>
2023-04-26 20:06:31 -0500

    Abstracted away loading the controllers

--- lib/src/features/invoices/presenation/edit_modal.dart
+++ lib/src/features/invoices/presenation/edit_modal.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:invoiceapp/constants/utils/random_nums.dart';
 import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
+import 'package:invoiceapp/src/features/shared/models/load_form_models.dart';
 import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_from_model.dart';
 import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/bill_to_model.dart';
 import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/item_list_model.dart';
-import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/domain/list_item_model.dart';
-import 'package:invoiceapp/src/features/shared/widgets/invoice_form_screen/presentation/list_item.dart';
 import 'package:invoiceapp/src/features/shared/widgets/modal_button.dart';
 import 'package:invoiceapp/theme/source_of_truth.dart';
 import 'package:invoiceapp/theme/theme.dart';
@@ -39,30 +37,8 @@ class EditModal {
                     onPressed:
                         // TODO: Loading controllers and navigating to the edit screen happens from the invocie screen and view invocie screen so should be shared somehow
                         () {
-                      billFromModel.loadControllers(invoice);
-                      billToModel.loadControllers(invoice);
-
-                      for (var item in invoice.listItems) {
-                        final model = CustomListItemModel();
-
-                        model.loadControllers(item);
-                        final String index =
-                            RandomRange.integer(1001, 999999).toString();
-                        final Map<String, CustomListItemModel> itemModelEntry =
-                            {index: model};
-
-                        final listItem = CustomListItem(
-                            index: index,
-                            onPress: itemsModel.removeItem,
-                            listItemModel: model);
-
-                        itemsModel.addItem({index: listItem});
-                        itemsModel.addItemModel(itemModelEntry);
-                      }
-
-                      print('list items: ${invoice.listItems}');
-                      context.push('/editInvoice', extra: invoice.docId);
-                      Navigator.pop(context);
+                      LoadFormControllers.load(context, billFromModel,
+                          billToModel, itemsModel, invoice);
                     },
                   ),
                 ),
