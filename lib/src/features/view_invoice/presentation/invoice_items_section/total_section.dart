ffe35cbfe1c219ec9f2da8e5569980b0c286d8bb
Kwe <Kweayon@gmail.com>
2023-04-26 20:27:27 -0500

    Refactor

--- lib/src/features/view_invoice/presentation/invoice_items_section/total_section.dart
+++ lib/src/features/view_invoice/presentation/invoice_items_section/total_section.dart
@@ -3,37 +3,43 @@ import 'package:invoiceapp/theme/theme.dart';
 
 class TotalSection extends StatelessWidget {
   final double total;
+
   const TotalSection({required this.total, super.key});
 
   @override
-  Widget build(BuildContext context) => Container(
-        height: 46,
-        decoration: BoxDecoration(
-          color: CustomTheme.otherColors['blue0'],
-          borderRadius: const BorderRadius.only(
-            bottomLeft: Radius.circular(12),
-            bottomRight: Radius.circular(12),
-          ),
+  Widget build(BuildContext context) {
+    const double radiusSize = 12;
+    const double padding = 12;
+    const double fontSize = 18;
+    return Container(
+      height: 46,
+      decoration: BoxDecoration(
+        color: CustomTheme.otherColors['blue0'],
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(radiusSize),
+          bottomRight: Radius.circular(radiusSize),
         ),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 12, right: 12),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              const Text(
-                'Total',
-                style: TextStyle(color: Colors.white, fontSize: 18),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: padding, right: padding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            const Text(
+              'Total',
+              style: TextStyle(color: Colors.white, fontSize: fontSize),
+            ),
+            Text(
+              '\$${total.toString()}',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: fontSize,
+                fontWeight: FontWeight.w500,
               ),
-              Text(
-                '\$${total.toString()}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
+  }
 }
