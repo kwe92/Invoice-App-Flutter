092c64ec7a20401c185631e9aab861dc28d2cfe4
Kwe <Kweayon@gmail.com>
2023-04-26 20:22:17 -0500

    Refactor

--- lib/src/features/invoices/presenation/invoice_screen.dart
+++ lib/src/features/invoices/presenation/invoice_screen.dart
@@ -1,44 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/constants/widgets/base_scaffold/base_scaffold.dart';
-import 'package:invoiceapp/src/features/invoices/domain/invoices_model.dart';
-import 'package:invoiceapp/src/features/invoices/presenation/edit_modal.dart';
-import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/invoice_list_tile.dart';
-import 'package:invoiceapp/src/features/invoices/presenation/title_icon_list_tile.dart';
-import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
-import 'package:provider/provider.dart';
+import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_view.dart';
 
 class InvoiceScreen extends StatelessWidget {
-  static const baseScaffoldKey = Key('baseScaffoldKey');
-  static const titleIconListTileKey = Key('titleIconListTileKey');
-  static const listViewKey = Key('listViewKey');
-
   const InvoiceScreen({super.key});
 
   @override
   Widget build(BuildContext context) => const BaseScaffold(
-      key: InvoiceScreen.baseScaffoldKey, body: _InvoicesListView());
-}
-
-// TODO: Needs to be its own widget
-
-class _InvoicesListView extends StatelessWidget {
-  const _InvoicesListView({super.key});
-  @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.only(top: 24.0),
-        child: Consumer(
-          builder: (BuildContext context, InvoicesModel model, _) => ListView(
-            key: InvoiceScreen.listViewKey,
-            children: <Widget>[
-              const TitleIconListTile(
-                key: InvoiceScreen.titleIconListTileKey,
-              ),
-              for (final InvoiceFormModel invoice in model.invoices.values)
-                GestureDetector(
-                    onLongPress: () => EditModal.modal(context, invoice),
-                    child: InvoiceListTile(invoice: invoice)),
-            ],
-          ),
-        ),
+        body: InvoicesListView(),
       );
 }
