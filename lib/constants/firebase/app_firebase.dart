e7e89ee35e39c6e53eeb52798b5f86151005fa18
Kwe <Kweayon@gmail.com>
2023-05-06 09:06:01 -0500

    Added Comments

--- lib/constants/firebase/app_firebase.dart
+++ lib/constants/firebase/app_firebase.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
 
+// TODO: Figureout how to track when user changed and update the invoices from firebase
 class AppFirebase {
   const AppFirebase();
 
