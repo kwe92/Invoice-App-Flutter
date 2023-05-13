9964f765e0ae7d5a44f1a276898c0699d11045cd
Kwe <Kweayon@gmail.com>
2023-05-13 14:40:47 -0500

    Update

--- lib/src/features/invoices/domain/filter_button_model.dart
+++ lib/src/features/invoices/domain/filter_button_model.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 class FilterButtonModel extends ChangeNotifier {
-  static final List<String> list = <String>['all', 'pending', 'paid'];
-  String dropdownValue = list.first;
+  static final List<String> dropdownOptions = <String>['all', 'pending', 'paid'];
+  String dropdownValue = dropdownOptions.first;
 
   void setValue(String value) {
     dropdownValue = value;
