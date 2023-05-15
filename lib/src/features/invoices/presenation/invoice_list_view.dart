a9a904574814a8ecf894f3bf1d200fc79ad4c1c1
Kwe <Kweayon@gmail.com>
2023-05-15 15:57:39 -0500

    Updates

--- lib/src/features/invoices/presenation/invoice_list_view.dart
+++ lib/src/features/invoices/presenation/invoice_list_view.dart
@@ -4,7 +4,6 @@ import 'package:invoiceapp/src/features/invoices/domain/invoices_model.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/edit_modal.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_error_screen.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/invoice_list_tile.dart';
-import 'package:invoiceapp/src/features/invoices/presenation/invoice_screen.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/title_icon_list_tile.dart';
 import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
 import 'package:provider/provider.dart';
