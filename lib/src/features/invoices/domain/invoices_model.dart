a0686aed9189eb944269b24448d19abdd3fb8f70
Kwe <Kweayon@gmail.com>
2023-06-11 13:57:30 -0500

    loaded InvoiceFormRecord from firebase data

--- lib/src/features/invoices/domain/invoices_model.dart
+++ lib/src/features/invoices/domain/invoices_model.dart
@@ -5,6 +5,8 @@ import 'package:flutter/foundation.dart';
 import 'package:invoiceapp/constants/enums/hash_keys.dart';
 import 'package:invoiceapp/constants/firebase/app_firebase.dart';
 import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
+import 'package:invoiceapp/src/features/shared/records/get_records.dart';
+// TODO: invoice.data() needs to be broken out into another map seperating BillTo and BillFrom data
 
 class InvoicesModel extends ChangeNotifier {
   static Map _invoices = {};
@@ -17,9 +19,16 @@ class InvoicesModel extends ChangeNotifier {
     _init();
   }
   void _invoicesCallback(QuerySnapshot snapshot) {
+    final invoiceRecord = CreateRecords.invoiceFormRecord(snapshot.docs[0].data() as Map<String, dynamic>);
+
+    print("Invoice from Record: $invoiceRecord");
+
     _invoices = {
       for (final invoice in snapshot.docs as List<QueryDocumentSnapshot<Map?>>)
-        invoice.data()![HashKeys.invoiceId.name]: InvoiceFormModel.fromJSON(
+        invoice.data()![HashKeys.invoiceId.name]:
+            // TODO: Replace InvoiceFormModel.fromJSON with CreateRecords.invoiceFormRecord
+
+            InvoiceFormModel.fromJSON(
           invoice.data() as Map<String, dynamic>,
         )
     };
