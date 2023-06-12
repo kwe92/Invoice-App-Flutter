5d229b18673cf161d344cedb6e7ea27443597c52
Kwe <Kweayon@gmail.com>
2023-06-12 17:39:50 -0500

    Update

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:invoiceapp/src/features/shared/records/records.dart';
 
-// TODO: Turn theme into a Record??
-
 class CustomTheme {
   const CustomTheme();
 
