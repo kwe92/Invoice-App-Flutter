a969296ac218c2a2eba84bb78fe9387863d41cdd
Kwe <Kweayon@gmail.com>
2023-06-12 11:51:55 -0500

    InvoiceFormRecord Implementation added

--- lib/src/features/invoices/presenation/invoice_list_tile/list_tile_body.dart
+++ lib/src/features/invoices/presenation/invoice_list_tile/list_tile_body.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/list_tile_decoration.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/list_tile_left_side.dart';
 import 'package:invoiceapp/src/features/invoices/presenation/invoice_list_tile/list_tile_right_side.dart';
-import 'package:invoiceapp/src/features/shared/models/invoice_form_model.dart';
+import 'package:invoiceapp/src/features/shared/records/records.dart';
 
 class InvoiceListTileBody extends StatelessWidget {
-  final InvoiceFormModel invoice;
+  final InvoiceFormRecord invoice;
   const InvoiceListTileBody({required this.invoice, super.key});
 
   @override
@@ -16,10 +16,7 @@ class InvoiceListTileBody extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 17.25),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              ListTileLeftSide(invoice: invoice),
-              ListTileRightSide(invoice: invoice)
-            ],
+            children: <Widget>[ListTileLeftSide(invoice: invoice), ListTileRightSide(invoice: invoice)],
           ),
         ),
       );
