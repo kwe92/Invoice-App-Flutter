bddc2debb422953bca660176e709539085ca81cd
Kwe <Kweayon@gmail.com>
2023-06-12 12:16:55 -0500

    added ThemeShadesRecord && OtherColorsRecord types

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:invoiceapp/src/features/shared/records/records.dart';
 
 // TODO: Turn theme into a Record??
 
@@ -13,29 +14,27 @@ class CustomTheme {
 
   static const Size appBarSize = Size.fromHeight(appBarHeight);
 
-  // TODO: create record for all colors
+  static const ThemeShadesRecord darkColors = (
+    shade0: Color.fromRGBO(12, 14, 22, 1),
+    shade1: Color.fromRGBO(20, 22, 37, 1),
+    shade2: Color.fromRGBO(30, 33, 57, 1),
+    shade3: Color.fromRGBO(37, 41, 69, 1),
+  );
 
-  static const Map<String, Color> darkColors = {
-    'shade0': Color.fromRGBO(12, 14, 22, 1),
-    'shade1': Color.fromRGBO(20, 22, 37, 1),
-    'shade2': Color.fromRGBO(30, 33, 57, 1),
-    'shade3': Color.fromRGBO(37, 41, 69, 1),
-  };
+  static const ThemeShadesRecord lightColors = (
+    shade0: Color.fromRGBO(126, 136, 195, 1),
+    shade1: Color.fromRGBO(136, 142, 176, 1),
+    shade2: Color.fromRGBO(223, 227, 250, 1),
+    shade3: Color.fromRGBO(248, 248, 251, 1),
+  );
 
-  static const Map<String, Color> lightColors = {
-    'shade0': Color.fromRGBO(126, 136, 195, 1),
-    'shade1': Color.fromRGBO(136, 142, 176, 1),
-    'shade2': Color.fromRGBO(223, 227, 250, 1),
-    'shade3': Color.fromRGBO(248, 248, 251, 1),
-  };
-
-  static const Map<String, Color> otherColors = {
-    'purple0': Color.fromRGBO(124, 93, 250, 1),
-    'purple1': Color.fromRGBO(146, 119, 255, 1),
-    'red0': Color.fromRGBO(236, 87, 87, 1),
-    'red1': Color.fromRGBO(255, 151, 151, 1),
-    'blue0': Color.fromRGBO(18, 103, 177, 1)
-  };
+  static const OtherColorsRecord otherColors = (
+    purple0: Color.fromRGBO(124, 93, 250, 1),
+    purple1: Color.fromRGBO(146, 119, 255, 1),
+    red0: Color.fromRGBO(236, 87, 87, 1),
+    red1: Color.fromRGBO(255, 151, 151, 1),
+    blue0: Color.fromRGBO(18, 103, 177, 1)
+  );
 
 // Created a custom MaterColor and can be used anywhere
   static MaterialColor createMaterialColor(Color color) {
