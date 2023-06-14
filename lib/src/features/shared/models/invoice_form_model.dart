9539466dafbe083bc51ab0060a8e9a4f813b8fa1
Kwe <Kweayon@gmail.com>
2023-06-13 22:48:48 -0500

    Refactor

--- lib/src/features/shared/models/invoice_form_model.dart
+++ lib/src/features/shared/models/invoice_form_model.dart
@@ -1,4 +1,5 @@
 import 'package:invoiceapp/constants/enums/hash_keys.dart';
+import 'package:invoiceapp/src/features/shared/records/records.dart';
 
 class InvoiceFormModel {
   final String invoiceId;
@@ -8,7 +9,7 @@ class InvoiceFormModel {
   final DateTime paymentDue;
   final String status;
   final Map<String, dynamic> billToText;
-  final Map<String, dynamic> billFromText;
+  final BillFromRecord billFromText;
   final List listItems;
   final double total;
 
@@ -24,18 +25,17 @@ class InvoiceFormModel {
       required this.listItems,
       required this.total});
 
-  factory InvoiceFormModel.fromJSON(Map<String, dynamic> json) =>
-      InvoiceFormModel(
-          invoiceId: json[HashKeys.invoiceId.name],
-          userId: json[HashKeys.userId.name],
-          docId: json[HashKeys.docId.name],
-          createdAt: json[HashKeys.createdAt.name].toDate(),
-          paymentDue: json[HashKeys.paymentDue.name].toDate(),
-          status: json[HashKeys.status.name],
-          billFromText: json[HashKeys.billFromText.name],
-          billToText: json[HashKeys.billToText.name],
-          listItems: json[HashKeys.listItems.name],
-          total: json[HashKeys.total.name]);
+  factory InvoiceFormModel.fromJSON(Map<String, dynamic> json) => InvoiceFormModel(
+      invoiceId: json[HashKeys.invoiceId.name],
+      userId: json[HashKeys.userId.name],
+      docId: json[HashKeys.docId.name],
+      createdAt: json[HashKeys.createdAt.name].toDate(),
+      paymentDue: json[HashKeys.paymentDue.name].toDate(),
+      status: json[HashKeys.status.name],
+      billFromText: json[HashKeys.billFromText.name],
+      billToText: json[HashKeys.billToText.name],
+      listItems: json[HashKeys.listItems.name],
+      total: json[HashKeys.total.name]);
 
   Map<String, dynamic> toMap() => {
         HashKeys.invoiceId.name: invoiceId,
